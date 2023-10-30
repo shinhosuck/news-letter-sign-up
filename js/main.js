9657d185c2f71bd59f4480d27282719fbf21dd27
shinhosuck <shinhosuck1973@gmail.com>
2023-10-31 02:18:08 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+// INDEX.HTML
+const form = document.querySelector('#form')
+const email = document.querySelector('#email')
+const errorText = document.querySelector('.error-text')
+const regex = /[\\\/:*?"<>|]+/;
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const emailValue = email.value.split('')
+    const singleChar = emailValue.find((char) => {
+        if(char === '@'){
+        return char
+        }
+    })
+
+    if(singleChar) {
+        if(regex.test(emailValue.toString())){
+        email.className = 'error'
+        errorText.style.display = 'flex'
+        }
+        else {
+            form.reset()
+            window.location.href = '/templates/success.html'
+        }
+    }
+    else {
+        email.className = 'error'
+        errorText.style.display = 'flex'
+    }
+})  
+
+email.addEventListener('click', (e) => {
+    if(e.currentTarget.classList.contains('error')) {
+    e.currentTarget.classList.remove('error')
+    errorText.style.display = 'none'
+    }
+})
+
